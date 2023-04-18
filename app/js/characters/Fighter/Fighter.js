927f4d3436a9d5a33f510e49ae758dc8a9372251
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-04-19 01:52:57 +0200

    Remove # from filterDexterity function

--- app/js/characters/Fighter/Fighter.js
+++ app/js/characters/Fighter/Fighter.js
@@ -13,7 +13,7 @@ class Fighter extends Character {
     return `${super.communicate()}First I hit and then I ask`;
   }
 
-  #filterDexterity(dexterity) {
+  filterDexterity(dexterity) {
     if (dexterity < 0) {
       return 0;
     }
