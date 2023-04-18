a7cc0b66704894cfc9ebce37b7293d49ae727076
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-04-19 01:55:14 +0200

    Add index.js

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,13 @@
-import Character from "./components/characters/Character/Character.js";
+import { characters } from "./Characters/Character.js";
+import CharacterCard from "./components/CharacterCard/CharacterCard.js";
+import List from "./components/List/List.js";
+import ListItem from "./components/ListItem/ListItem.js";
+
+const app = document.querySelector(".app");
+
+const list = new List(app, "characters-list row list-unstyled");
+
+characters.forEach((character) => {
+  const listItem = new ListItem(list.element, "character col");
+  new CharacterCard(listItem.element, character);
+});
